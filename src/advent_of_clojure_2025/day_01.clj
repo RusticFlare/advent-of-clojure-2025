f920051eb6af29cc7e6683afc79b93c6dba7af5d
rusticflare <5823777+RusticFlare@users.noreply.github.com>
2025-12-14 08:28:37 +0000

    Ran `cljfmt fix .`

--- src/advent_of_clojure_2025/day_01.clj
+++ src/advent_of_clojure_2025/day_01.clj
@@ -23,27 +23,26 @@
   [input]
   (case (get input 0)
     \L (Left. (Integer/parseInt (subs input 1)))
-    \R (Right. (Integer/parseInt (subs input 1)))
-    ))
+    \R (Right. (Integer/parseInt (subs input 1)))))
 
 (defn part-1
   "Day 01 Part 1"
   [input]
   (->>
-    (str/split-lines input)
-    (map to-instruction)
-    (reductions (flip nextPosition) 50)
-    (filter zero?)
-    (count)))
+   (str/split-lines input)
+   (map to-instruction)
+   (reductions (flip nextPosition) 50)
+   (filter zero?)
+   (count)))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
   (->>
-    (str/split-lines input)
-    (map to-instruction)
-    (reductions (flip nextRange) (range 50 51))
-    (flatten)
-    (map #(mod % 100))
-    (filter zero?)
-    (count)))
+   (str/split-lines input)
+   (map to-instruction)
+   (reductions (flip nextRange) (range 50 51))
+   (flatten)
+   (map #(mod % 100))
+   (filter zero?)
+   (count)))
