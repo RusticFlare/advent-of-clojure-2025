f5365785208952478253bf8c4377fe4dea53fadc
rusticflare <5823777+RusticFlare@users.noreply.github.com>
2025-12-14 20:43:52 +0000

    Simplify `inclusive-range` and use `apply`

--- src/advent_of_clojure_2025/day_02.clj
+++ src/advent_of_clojure_2025/day_02.clj
@@ -1,14 +1,14 @@
 (ns advent-of-clojure-2025.day-02
   (:require [clojure.string :as str]))
 
-(defn inclusive-range [[start end]]
+(defn inclusive-range [start end]
   (range start (inc end)))
 
 (defn to-range [input]
   (let [values (->>
                 (str/split input #"-")
                 (map ^[String] Long/parseLong))]
-    (inclusive-range values)))
+    (apply inclusive-range values)))
 
 (defn duplicateSequence? [id]
   (re-matches #"(.+)\1" (Long/toString id)))
